2ffbf4a1f7144e8233373012262a77338b6ed7f9
bolanleazeemah@gmail.com <bolanleazeemah@gmail.com>
2023-09-29 02:25:59 +0000

    New changes

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,45 +1,69 @@
 #!/usr/bin/env python3
 """
-Main file
+Deletion-resilient hypermedia pagination
 """
 
-Server = __import__('3-hypermedia_del_pagination').Server
-
-# Add the missing line here
-def get_hyper_index(self, index: int = None, page_size: int = 10):
-    # Your existing method code here
-
-setattr(Server, 'get_hyper_index', get_hyper_index)  # Add this line to update the method
-
-server = Server()
-
-server.indexed_dataset()
-
-try:
-    server.get_hyper_index(300000, 100)
-except AssertionError:
-    print("AssertionError raised when out of range")
+import csv
+import math
+from typing import List, Dict
 
 
-index = 3
-page_size = 2
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
 
-print("Nb items: {}".format(len(server._Server__indexed_dataset)))
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
 
-# 1- request first index
-res = server.get_hyper_index(index, page_size)
-print(res)
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
 
-# 2- request next index
-print(server.get_hyper_index(res.get('next_index'), page_size))
+        return self.__dataset
 
-# 3- remove the first index
-del server._Server__indexed_dataset[res.get('index')]
-print("Nb items: {}".format(len(server._Server__indexed_dataset)))
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
 
-# 4- request again the initial index -> the first data retreives is not the same as the first request
-print(server.get_hyper_index(index, page_size))
+    def get_hyper_index(self, index: int = None,
+                        page_size: int = 10) -> Dict:
+        """ return all data"""
 
-# 5- request again initial next index -> same data page as the request 2-
-print(server.get_hyper_index(res.get('next_index'), page_size))
+        if index is None:
+            index = 0
 
+        # validate the index
+        assert isinstance(index, int)
+        assert 0 <= index < len(self.indexed_dataset())
+        assert isinstance(page_size, int) and page_size > 0
+
+        data = []  # collect all indexed data
+        next_index = index + page_size
+
+        for value in range(index, next_index):
+            if self.indexed_dataset().get(value):
+                data.append(self.indexed_dataset()[value])
+            else:
+                value += 1
+                next_index += 1
+
+        return {
+            'index': index,
+            'data': data,
+            'page_size': page_size,
+            'next_index': next_index
+        }
