d098d529d5b52dd3d728bea786c73f60b2c68b23
bolanleazeemah@gmail.com <bolanleazeemah@gmail.com>
2023-09-29 02:19:05 +0000

    New changes

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -3,28 +3,14 @@
 Main file
 """
 
-class Server:
-    def __init__(self):
-        # Initialize your server here
-        self.__indexed_dataset = {}  # Replace with your dataset initialization
+Server = __import__('3-hypermedia_del_pagination').Server
 
-    def indexed_dataset(self):
-        # Implement your indexed_dataset method here
-        pass
+# Add the missing line here
+def get_hyper_index(self, index: int = None, page_size: int = 10):
+    # Your existing method code here
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10):
-        # Modify the get_hyper_index method to accept index and page_size parameters
-        # with default values
-        if index is None:
-            # Handle the case when index is not provided
-            # Implement your logic here
-            pass
-        else:
-            # Handle the case when index is provided
-            # Implement your logic here
-            pass
+setattr(Server, 'get_hyper_index', get_hyper_index)  # Add this line to update the method
 
-# Create an instance of the Server class
 server = Server()
 
 server.indexed_dataset()
@@ -33,6 +19,7 @@ try:
     server.get_hyper_index(300000, 100)
 except AssertionError:
     print("AssertionError raised when out of range")
+
 
 index = 3
 page_size = 2
@@ -50,7 +37,7 @@ print(server.get_hyper_index(res.get('next_index'), page_size))
 del server._Server__indexed_dataset[res.get('index')]
 print("Nb items: {}".format(len(server._Server__indexed_dataset)))
 
-# 4- request again the initial index -> the first data retrieves are not the same as the first request
+# 4- request again the initial index -> the first data retreives is not the same as the first request
 print(server.get_hyper_index(index, page_size))
 
 # 5- request again initial next index -> same data page as the request 2-
