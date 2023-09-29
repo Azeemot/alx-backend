2557d6ccf0583063c41b9a66d35207411b541103
bolanleazeemah@gmail.com <bolanleazeemah@gmail.com>
2023-09-29 02:12:09 +0000

    New changes

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,69 +1,58 @@
 #!/usr/bin/env python3
 """
-Deletion-resilient hypermedia pagination
+Main file
 """
 
-import csv
-import math
-from typing import List, Dict
-
-
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
-    DATA_FILE = "Popular_Baby_Names.csv"
-
     def __init__(self):
-        self.__dataset = None
-        self.__indexed_dataset = None
+        # Initialize your server here
+        self.__indexed_dataset = {}  # Replace with your dataset initialization
 
-    def dataset(self) -> List[List]:
-        """Cached dataset
-        """
-        if self.__dataset is None:
-            with open(self.DATA_FILE) as f:
-                reader = csv.reader(f)
-                dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
+    def indexed_dataset(self):
+        # Implement your indexed_dataset method here
+        pass
 
-        return self.__dataset
-
-    def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
-        """
-        if self.__indexed_dataset is None:
-            dataset = self.dataset()
-            truncated_dataset = dataset[:1000]
-            self.__indexed_dataset = {
-                i: dataset[i] for i in range(len(dataset))
-            }
-        return self.__indexed_dataset
-
-    def get_hyper_index(self, index: int = None,
-                        page_size: int = 10) -> Dict:
-        """ return all data"""
-
+    def get_hyper_index(self, index: int = None, page_size: int = 10):
+        # Modify the get_hyper_index method to accept index and page_size parameters
+        # with default values
         if index is None:
-            index = 0
+            # Handle the case when index is not provided
+            # Implement your logic here
+            pass
+        else:
+            # Handle the case when index is provided
+            # Implement your logic here
+            pass
 
-        # validate the index
-        assert isinstance(index, int)
-        assert 0 <= index < len(self.indexed_dataset())
-        assert isinstance(page_size, int) and page_size > 0
+# Create an instance of the Server class
+server = Server()
 
-        data = []  # collect all indexed data
-        next_index = index + page_size
+server.indexed_dataset()
 
-        for value in range(index, next_index):
-            if self.indexed_dataset().get(value):
-                data.append(self.indexed_dataset()[value])
-            else:
-                value += 1
-                next_index += 1
+try:
+    server.get_hyper_index(300000, 100)
+except AssertionError:
+    print("AssertionError raised when out of range")
 
-        return {
-            'index': index,
-            'data': data,
-            'page_size': page_size,
-            'next_index': next_index
-        }
+index = 3
+page_size = 2
+
+print("Nb items: {}".format(len(server._Server__indexed_dataset)))
+
+# 1- request first index
+res = server.get_hyper_index(index, page_size)
+print(res)
+
+# 2- request next index
+print(server.get_hyper_index(res.get('next_index'), page_size))
+
+# 3- remove the first index
+del server._Server__indexed_dataset[res.get('index')]
+print("Nb items: {}".format(len(server._Server__indexed_dataset)))
+
+# 4- request again the initial index -> the first data retrieves are not the same as the first request
+print(server.get_hyper_index(index, page_size))
+
+# 5- request again initial next index -> same data page as the request 2-
+print(server.get_hyper_index(res.get('next_index'), page_size))
+
